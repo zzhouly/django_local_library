297c3bc1bd6ae83fe1000793c9f647a300530c95
zzhouly <zzhouly@gamil.com>
2021-06-24 13:52:36 -0700

    changed AlLOWED_HOSTS in settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['immense-retreat-17042.herokuapp.com',]
 
 
 # Application definition
