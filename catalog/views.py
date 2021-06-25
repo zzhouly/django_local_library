0c164f2a6dccccdc6d12f9596ea7921fd2bc77c8
zzhouly <zzhouly@gamil.com>
2021-06-25 15:48:27 -0700

    Debug in  catalog/views.py, change typo(PermissionRequiredMinxin->PermissionRequiredMixin)

--- catalog/views.py
+++ catalog/views.py
@@ -128,33 +128,33 @@ from django.urls import reverse_lazy
 
 from catalog.models import Author
 
-class AuthorCreate(PermissionRequiredMinxin, CreateView):
+class AuthorCreate(PermissionRequiredMixin, CreateView):
     model = Author
     fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
     # initial = {'date_of_death': '11/06/2020'}
     permission_required = 'catalog.can_mark_returned'
 
-class AuthorUpdate(PermissionRequiredMinxin, UpdateView):
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
     model = Author
     fields = '__all__' # Not recommended (potential security issue if more fields added)
     permission_required = 'catalog.can_mark_returned'
 
-class AuthorDelete(PermissionRequiredMinxin, DeleteView):
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
     model = Author
     success_url = reverse_lazy('authors')
     permission_required = 'catalog.can_mark_returned'
 
-class BookCreate(PermissionRequiredMinxin, CreateView):
+class BookCreate(PermissionRequiredMixin, CreateView):
     model = Book
     fields = '__all__'
     permission_required = 'catalog.can_mark_returned'
 
-class BookUpdate(PermissionRequiredMinxin, UpdateView):
+class BookUpdate(PermissionRequiredMixin, UpdateView):
     model = Book 
     fields = '__all__'
     permission_required = 'catalog.can_mark_returned'
 
-class BookDelete(PermissionRequiredMinxin, DeleteView):
+class BookDelete(PermissionRequiredMixin, DeleteView):
     model = Book 
     success_url = reverse_lazy('books')
     permission_required = 'catalog.can_mark_returned'
